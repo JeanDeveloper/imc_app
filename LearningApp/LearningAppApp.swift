84fc738cd78e9b7d62cc6e105645ecae3f7c03d0
Jean Carlo <jecachusa.1996@gmail.com>
2024-12-28 11:50:21 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Jean Carlo Chunga Sandoval on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningAppTests/LearningAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppTests.swift
+//  LearningAppTests
+//
+//  Created by Jean Carlo Chunga Sandoval on 28/12/24.
+//
+
+import Testing
+@testable import LearningApp
+
+struct LearningAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
